6e98bcb25e86788e9f74502c6fee96e363409d95
Steven T.A. Carter <steve.carter@gmail.com>
2021-10-21 17:32:19 -0700

    Gestures Part 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -24,12 +24,17 @@ const mapDispatchToProps = {
 
     const {campsite} = props;
 
+    const view = React.createRef();
+
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
         onPanResponderEnd: (e, gestureState) => {
-            console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
                 Alert.alert(
                     'Add Favorite',
@@ -59,6 +64,7 @@ const mapDispatchToProps = {
                 animation='fadeInDown' 
                 duration={2500} 
                 delay={1000}
+                ref={view}
                 {...panResponder.panHandlers}>
                     
                 <Card
