f195b2c00ad63787056c9fc5b561f1a1dd500ea0
Steven T.A. Carter <steve.carter@gmail.com>
2021-10-24 17:27:29 -0700

    Week 4 Workshop

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,8 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator'
+
 
 class LoginTab extends Component {
 
@@ -155,12 +157,38 @@ class RegisterTab extends Component {
         if (cameraPermission.status === 'granted' && cameraRollPermission.status === 'granted') {
             const capturedImage = await ImagePicker.launchCameraAsync({
                 allowsEditing: true,
-                aspect: [1, 1]
+                aspect: [1,1]
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                
+                this.processingImage(capturedImage.uri);
+                
+                
             }
+        }
+    }
+
+    processingImage = async(imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri, [{resize: {width:400}}], { format: ImageManipulator.SaveFormat.PNG }
+        );
+       console.log(processedImage);
+       this.setState({imageUrl: processedImage.uri});
+    }
+
+    getImagesFromGallery = async() =>{
+        const cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1,1]
+            });
+        if(!capturedImage.cancelled) {
+            console.log(capturedImage);
+            this.processingImage(capturedImage.uri);
+        }
         }
     }
 
@@ -190,6 +218,11 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        
+                       <Button
+                            title='Gallery'
+                            onPress={this.getImagesFromGallery}
                         />
                     </View>
                     <Input
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -332,12 +332,7 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        this.showNetInfo();
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
@@ -348,6 +343,14 @@ class Main extends Component {
         this.unsubscribeNetInfo();
     }
     
+    showNetInfo = async() => {
+        const connectionInfo =   await NetInfo.fetch();
+             (Platform.OS === 'ios') ? 
+                  Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                  : ToastAndroid.show('Initial Network Connectivity Type: ' + 
+                      connectionInfo.type, ToastAndroid.LONG);
+      }
+      
     handleConnectivityChange = connectionInfo => {
         let connectionMsg = 'You are now connected to an active network.';
         switch (connectionInfo.type) {
